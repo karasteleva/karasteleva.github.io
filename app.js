a05ca850a82e8f7dd6fd66663533892d12e857b4
karasteleva <70016326+karasteleva@users.noreply.github.com>
2020-08-21 14:15:07 +0300

    Test

---
+++ app.js
@@ -0,0 +1,39 @@
+//$(function() {
+//   $('#myButton').click(function() {
+//  $('#myDiv').toggle('slow', function() {
+//    // Animation complete.
+//  });
+//});
+//} 
+//});
+//function showCurs() {
+//   document.getElementById('Curs').style.display = "block";}
+//
+//
+//$(document).ready(function(){
+//    $("#showHideCurs").click(function () {
+//        if ($("#curs").is(":hide")) {
+//            $("#curs").show();
+//        } else {
+//            $("#curs").hide();
+//        }
+//        return false;
+//    });
+//});
+//
+//$(document).ready(function(){
+//    $("#showHideContent").click(function () {
+//        if ($("#content").is(":hidden")) {
+//            $("#content").show("slow");
+//        } else {
+//            $("#content").hide("slow");
+//        }
+//        return false;
+//    });
+//});
+//    
+
+//
+//<button id="showHideContent">Показать/Скрыть</button>
+//
+//<div id="content" style="display:none;">Содержание, которое скрыто по-умолчанию. Покажется/скроется при клике на одну и ту же ссылку.</div>
